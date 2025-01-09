8905fe8f27fad48a5d57eacce9bd9e04b54cc87f
Auqamas <auqamasm4@gmail.com>
2025-01-09 21:42:50 +0500

    1.3.3 Update

--- script.js
+++ script.js
@@ -313,3 +313,28 @@ gsap.from("#imgetext img", {
 // Team Page Codes
 // Global smooth scroll fix
 document.documentElement.style.scrollBehavior = 'smooth';
+
+
+function videoconAnimation() {
+    var videocon = document.querySelector("#video-container");
+    var playbtn = document.querySelector("#play");
+    videocon.addEventListener("mouseenter", function () {
+      gsap.to(playbtn, {
+        scale: 1,
+        opacity: 1,
+      });
+    });
+    videocon.addEventListener("mouseleave", function () {
+      gsap.to(playbtn, {
+        scale: 0,
+        opacity: 0,
+      });
+    });
+    document.addEventListener("mousemove", function (dets) {
+      gsap.to(playbtn, {
+        left: dets.x - 70,
+        top: dets.y - 80,
+      });
+    });
+  }
+  videoconAnimation();  
